3a98e76731bd2c3716484d8042911040917cd544
ITIvanov18 <59703243+ITIvanov18@users.noreply.github.com>
2025-12-01 20:25:00 +0200

    Initialize Spring Boot project and MySQL database connection

---
+++ src/main/java/nbu/edomoupravitel/EDomoupravitelApplication.java
@@ -0,0 +1,13 @@
+package nbu.edomoupravitel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDomoupravitelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDomoupravitelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nbu/edomoupravitel/EDomoupravitelApplicationTests.java
@@ -0,0 +1,13 @@
+package nbu.edomoupravitel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDomoupravitelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
